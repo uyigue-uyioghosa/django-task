83ac6928a6d34eef0c11f51b05df17fc913691e8
uyioghosa uyigue <uyigueuyioghosa@gmail.com>
2022-06-19 21:44:07 +0100

    initialcommit

---
+++ I4G013073KKR/blog/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User=get_user_model()
+class Post(models.Model):
+    Title=models.CharField(max_length=200)
+    Text=models.TextField()
+    Author=models.ForeignKey(User, on_delete=models.CASCADE)
